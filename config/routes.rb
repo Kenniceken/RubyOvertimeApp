27d9b9ed539f6dbaba412a5a602f6935876488a6
Kennice Ken <kennice_offor@gmail.com>
2020-12-29 17:33:16 +0800

    Added navigation menu with logout and Settings for user Profile

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   resources :posts
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   root to: 'static#homepage'
 end
